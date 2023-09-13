a08ff54d501e907cd8311bf1b4297895ca5ff750
sasasan03 <sako.hiro62@gmail.com>
2023-09-13 22:26:26 +0900

    first inti

---
+++ OnApperTest/OnApperTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnApperTestApp.swift
+//  OnApperTest
+//
+//  Created by sako0602 on 2023/09/13.
+//
+
+import SwiftUI
+
+@main
+struct OnApperTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
